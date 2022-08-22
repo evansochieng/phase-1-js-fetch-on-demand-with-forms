0567ae0a43fc74aab302e8485bd98c6ea8384a7d
Evans Ochieng <evansochieng121@gmail.com>
2022-08-22 21:21:10 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form'); //grab the form element
+
+    // add an event listener
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        //console.log(event.target.children[1].value) //gert value of input from event
+        const input = document.querySelector('input#searchByID');//get value of input directly
+
+        // Fetch data based on User Input
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+            
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
